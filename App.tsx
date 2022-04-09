70b4b176aa20973d07153142fd5e411baff96e3b
wicked-knife <1353640486@qq.com>
2022-04-09 23:50:28 +0800

    feat: add init code

--- App.tsx
+++ App.tsx
@@ -1,22 +1,26 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Text} from 'react-native';
 import axios from '@API/base';
 
 export default function App() {
+  const [songs, setSongs] = useState([]);
   useEffect(() => {
-    console.log('App.tsx');
     axios
       .get(
         'https://netease-cloud-music-api-eight-eosin.vercel.app/top/playlist?limit=10',
       )
       .then(res => {
-        console.log(res.data);
+        setSongs(res.data.playlists.map((item: any) => item.name));
       });
-  });
+  }, []);
+
+  console.log(songs);
   return (
     <NavigationContainer>
-      <Text>ss2s</Text>
+      {songs.map(song => (
+        <Text key={song}>{song}</Text>
+      ))}
     </NavigationContainer>
   );
 }
