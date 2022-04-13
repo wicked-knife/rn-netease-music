607194aaef2ba866d60bff0940b1ef1f438cf5fc
wicked-knife <1353640486@qq.com>
2022-04-13 21:15:03 +0800

    simple demo

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView} from 'react-native'
 import React from 'react'
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <ScrollView style={{flex: 1}}>
+      <ScrollView nestedScrollEnabled style={{height: 400, backgroundColor: 'green'}}>
+        <View style={styles.block} />
+        <View style={styles.block} />
+        <View style={styles.block} />
+        <View style={styles.block} /><View style={styles.block} /><View style={styles.block} /><View style={styles.block} /><View style={styles.block} /><View style={styles.block} /><View style={styles.block} />
+      </ScrollView>
+      <View style={styles.block} />
+      <View style={styles.block} />
+      <View style={styles.block} />
+      <View style={styles.block} />
+      <View style={styles.block} />
+      <View style={styles.block} />
+    </ScrollView>
   )
 }
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  block: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+    marginBottom: 20
+  }
+})
