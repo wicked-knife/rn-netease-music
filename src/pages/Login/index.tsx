b2b2c8ea48ba25ee180f2f7be5250b74a2d929d0
wicked-knife <1353640486@qq.com>
2022-04-20 23:19:42 +0800

    Revert "simple demo"
    This reverts commit 607194aaef2ba866d60bff0940b1ef1f438cf5fc.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,32 +1,14 @@
-import { StyleSheet, Text, View, ScrollView} from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const Login = () => {
   return (
-    <ScrollView style={{flex: 1}}>
-      <ScrollView nestedScrollEnabled style={{height: 400, backgroundColor: 'green'}}>
-        <View style={styles.block} />
-        <View style={styles.block} />
-        <View style={styles.block} />
-        <View style={styles.block} /><View style={styles.block} /><View style={styles.block} /><View style={styles.block} /><View style={styles.block} /><View style={styles.block} /><View style={styles.block} />
-      </ScrollView>
-      <View style={styles.block} />
-      <View style={styles.block} />
-      <View style={styles.block} />
-      <View style={styles.block} />
-      <View style={styles.block} />
-      <View style={styles.block} />
-    </ScrollView>
+    <View>
+      <Text>Login</Text>
+    </View>
   )
 }
 
 export default Login
 
-const styles = StyleSheet.create({
-  block: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
-    marginBottom: 20
-  }
-})
+const styles = StyleSheet.create({})
